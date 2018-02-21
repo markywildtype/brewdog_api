f67b874de324a97ec4d923107bc4c7a489c1ea81
Mark Blanford <markytorture@hotmail.com>
2018-02-21 17:34:46 +0000

    API call successful

--- public/app.js
+++ public/app.js
@@ -1,5 +1,31 @@
 const app = function () {
+  const url = "https://api.punkapi.com/v2/beers"
+  requestBeerData(url, requestComplete);
+
+  const beerList = document.getElementById('beer-list');
 
 };
+
+
+//API Request Handling:
+const requestBeerData = function(url, callback){
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.addEventListener('load', callback);
+  request.send();
+}
+
+const requestComplete = function(){
+  if(this.status !== 200) return;
+  const jsonString = this.responseText;
+  const beerArray = JSON.parse(jsonString);
+  populateBeerList(beerArray);
+}
+
+//Data handling
+const populateBeerList = function(){
+
+}
+
 
 document.addEventListener('DOMContentLoaded', app);
